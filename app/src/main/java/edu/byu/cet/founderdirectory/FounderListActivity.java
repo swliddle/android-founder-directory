b103431772b5186b9d582bae4e0e4bd981302eab
Steve Liddle <liddle@byu.edu>
2016-03-21 14:00:06 -0600

    Cleaned up previous commit.

--- app/src/main/java/edu/byu/cet/founderdirectory/FounderListActivity.java
+++ app/src/main/java/edu/byu/cet/founderdirectory/FounderListActivity.java
@@ -1,14 +1,6 @@
 package edu.byu.cet.founderdirectory;
 
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
